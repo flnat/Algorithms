8f026051fce6ae5aad6e57f3bd8ec39fa7131a85
Florian Natterer <natterer.flo99@gmail.com>
2022-06-08 11:14:02 +0200

    Implemented Bag class

---
+++ src/data_structures/Bag.java
@@ -0,0 +1,58 @@
+package data_structures;
+
+import java.util.Iterator;;
+
+public class Bag<Item> implements Iterable<Item> {
+
+    private Node first;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+    public Bag(){}
+
+    public Bag(Item item){
+        this.first = new Node();
+        this.first.item = item;
+        this.first.next = null; 
+    }
+
+    public Bag(Iterable<Item> items){
+
+        for (Item item: items){
+            add(item);
+        }
+    }
+
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+    }
+
+}
