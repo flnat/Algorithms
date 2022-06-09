80b75c2156071dd8e66e558c2899e58a47f51dd4
Florian Natterer <natterer.flo99@gmail.com>
2022-06-09 21:48:48 +0200

    Changes to symbolTable

--- src/data_structures/symbol_tables/SymbolTable.java
+++ src/data_structures/symbol_tables/SymbolTable.java
@@ -1,18 +1,33 @@
 package data_structures.symbol_tables;
 
 interface SymbolTable<Key extends Comparable<Key>, Value> {
-    
+
     public void put(Key key, Value val);
+
     public Value get(Key key);
+
     public void delete(Key key);
-    default public boolean contains(Key key){
+
+    public void deleteMin();
+
+    default public boolean contains(Key key) {
         return get(key) != null;
     }
+
     public int size();
+
     public Iterable<Key> keys();
-    default public boolean isEmpty(){
+
+    default public boolean isEmpty() {
         return size() == 0;
     }
 
+    public Key min();
+
+    public Key floor();
+
+    public Key select();
+
+    public int rank();
 
 }
