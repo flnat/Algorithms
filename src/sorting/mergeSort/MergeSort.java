830d52e3a9d026c7f68a41d94472f280ab0f40c3
Florian Natterer <natterer.flo99@gmail.com>
2022-06-10 19:48:02 +0200

    fixed some bugs in MergeSort and added some performance tests in the client

--- src/sorting/mergeSort/MergeSort.java
+++ src/sorting/mergeSort/MergeSort.java
@@ -4,11 +4,29 @@ import sorting.genericSorting;
 
 public class MergeSort extends genericSorting {
     private static Comparable[] aux;
+    
+    public static void sort(Comparable[] a){
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
 
-    public static <T extends Comparable<T>> void merge(Comparable[] a, int low, int mid, int high) {
+    private static void sort(Comparable[] a, int low, int high){
+        if (high <= low){
+            return;
+        }
+        int mid = low + (high - low) / 2;
+        sort(a, low, mid);
+        sort(a, mid + 1, high);
+        merge(a, low, mid, high);
+    }
+
+
+
+
+    public static void merge(Comparable[] a, int low, int mid, int high) {
         int i = low;
         int j = mid + 1;
-        for (int k = 0; k <= high; k++) {
+        for (int k = low; k <= high; k++) {
             aux[k] = a[k];
         }
         for (int k = low; k <= high; k++) {
